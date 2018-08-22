057f354793497d3ac8422c645b92c2e3e7858f6a
Judith van Stegeren <git@jd7h.com>
2018-08-22 21:01:55 +0200

    Refactor code

--- src/spotifail.py
+++ src/spotifail.py
@@ -17,32 +17,35 @@ def get_spotify(filename):
     return spotify
 
 def get_stats(filename):
-    data = []
+    rawdata = []
     with open(filename,"r") as infile:
-      data = infile.readlines()
+      rawdata = infile.readlines()
+    len(rawdata)
+    data = []
+    for d in rawdata:
+      data.append(d.strip().split(","))
     len(data)
-    data2 = []
-    for d in data:
-      data2.append(d.strip().split(","))
-    len(data2)
-    return data2
+    return data
 
 def top_n(data,n):
     tracks = [] # concatenated, no timestamps
+    # last column of csv contains timestamps (we don't need those)
+    # Counter cannot deal with list-objects, that's why I join the strings
     for d in data:
         tracks.append(";".join(d[:-1]))
-    top = Counter(tracks).most_common(n)
+    top = Counter(tracks).most_common(n) 
     toplist = []
     for t in top:
         toplist.append(t[0].split(";"))
     return toplist
 
+# quick n dirty main function that prints what I need
 def search_spotify(spotify,toplist):      
     notfound = 0
     for tr in toplist:
        try:
-         results = spotify.search(q=" ".join(tr), type='track')
-         sleep(0.1)
+         results = spotify.search(q=" ".join(tr).lower(), type='track')
+         sleep(0.1) # max 10 queries per second is more than enough
          name = results['tracks']['items'][0]['name']
        except Exception as e:
          print(e)
